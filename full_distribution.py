cd08497860cb9a512e9941f63d33dd58e7e0a986
Qihang Zhang <qihangz.work@gmail.com>
2025-11-18 22:16:36 +0000

    Refactor full_distribution.py to improve temperature setting and utilize vocab_size for top_logprobs_num

--- full_distribution.py
+++ full_distribution.py
@@ -3,27 +3,39 @@ from pprint import pprint
 from transformers import AutoTokenizer
 from math import exp
 
+def check_if_all_index_included(full_logprobs, vocab_size):
+    index_list = [logprob[1] for logprob in full_logprobs]
+    
+    return_value = True
+    for i in range(vocab_size):
+        if i not in index_list:
+            print(f"Missing index: {i}")
+            return_value = False
+    
+    return return_value
+
 if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3.1-8B-Instruct")
+    vocab_size = tokenizer.vocab_size
     url = "http://localhost:30000/generate"
     
     payload = {
         "model": "llama3.1-8b",
         "text": "The capital of France is",
         "sampling_params": {
-            "temperature": 1.0,
+            "temperature": 0.75,
             "max_new_tokens": 0,
             "ignore_eos": False,
         },
         "return_logprob": True,
         "logprob_start_len": 0,
-        "top_logprobs_num": 5,
+        "top_logprobs_num": vocab_size,
     }
 
     r = requests.post(url, json=payload, timeout=60)
     r.raise_for_status()
     data = r.json()
-    pprint(data)
+    # pprint(data)
     
     input_token_ids = []
     for i in range(len(data["meta_info"]["input_token_logprobs"])):
@@ -35,9 +47,12 @@ if __name__ == "__main__":
         
     full_logprobs = data["meta_info"]["input_top_logprobs"]
     
-    print("Full distribution for input tokens:", full_logprobs)
-    import ipdb; ipdb.set_trace()
+    # print("Full distribution for input tokens:", full_logprobs)
+
     for i in range(1, len(full_logprobs)):
         logprob = [logprobs[0] for logprobs in full_logprobs[i]]
         prob = [exp(lp) for lp in logprob]
         print(f"Token Position Index: {i}, Probabilities Sum: {sum(prob)}")
+        
+        all_included = check_if_all_index_included(full_logprobs[i], vocab_size)
+        print(f"All indices included: {all_included}")
